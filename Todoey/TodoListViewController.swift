5e3379f5c6717f51365146117ff235ce4387c03e
Nic le Roux <nicleroux@hotmail.com>
2018-09-14 14:42:42 +0200

    Persist data using UserDefaults method.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Pet Squirrel", "Kick Doge", "Punch Goat"]
+    var itemArray : [String] = []
+    
+    let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = userDefaults.array(forKey: "todoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK: TableView DataSource Methods
@@ -30,6 +35,7 @@ class TodoListViewController: UITableViewController {
     }
 
     //MARK: TableView Delegate Methods
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -39,7 +45,30 @@ class TodoListViewController: UITableViewController {
         }
 
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //Mark: Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what happens after add is pressed on UI Alert
+            self.itemArray.append(textField.text!)
+            self.userDefaults.setValue(self.itemArray, forKey: "todoListArray")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            textField = alertTextField
+            textField.placeholder = "Create new item"
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
